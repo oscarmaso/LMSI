61760f55bc8409eb5c12e99f6f37c2c72bad913d
Unknown <joseluisdamdosa@gmail.com>
2017-05-29 21:24:15 +0200

    Modificaciones a 29/05/2017
    Modificaciones a 29/05/2017

--- Curso 16-17/javascript/script/05 script.js
+++ Curso 16-17/javascript/script/05 script.js
@@ -7,10 +7,16 @@ function CalcularRango()
 
 	res = document.getElementById('resultado');
 	edad = document.getElementById('edadActual');
-	numeroEdad = parseInt(edad.innerHTML);
+	numeroEdad = parseInt(edad.value);
 	if((numeroEdad>=0) && (numeroEdad<=12))
 		res.innerHTML = "En la infancia";
 	else 
 	  if ((numeroEdad>=13) && (numeroEdad<=17))
 		res.innerHTML = "En la adolescencia";
+	  else
+	  	if ((numeroEdad>=18) && (numeroEdad<=30))
+		  res.innerHTML = "En la juventud";
+	  	else
+	  	  res.innerHTML = "En la edad adulta";
+
 }
